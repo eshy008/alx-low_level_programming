9d433b18508712326f7927b53bed9825ed0b6a05
John <eshy008@gmail.com>
2022-10-13 18:11:44 -0700

    4-puts.c edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
-#!/bin/bash
+/*
+ * File: 4-puts.c
+ * Author: John Ifoga
+ */
+
 #include <stdio.h>
+
+/*
+ * main - Prints "Programming is like building a multilingual
+ *		  puzzle, followed by a new line.
+ *
+ * Return: always 0;
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
