6d4974dbd05b524b24bd3aa1bef0479bc3b93529
John <eshy008@gmail.com>
2022-10-13 18:21:37 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
-#!/bin/bash
+/*
+ * File : 5-printf.c
+ * Auth : John ifoga
+ */
+
 #include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art," 
+ * followed by a new line.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
