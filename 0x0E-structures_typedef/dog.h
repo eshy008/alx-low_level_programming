24d50397f73de32c3a9956336a901add8a5022a8
john <eshy008@gmail.com>
2022-11-14 14:35:11 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,29 +1,26 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _dog_h_
+#define _dog_h_
 
 /**
- * struct dog - dog structure
- *
- * @name: name of dog
- * @age: age of the dog
- * @owner: dog owner
- *
-*/
-struct dog
+ * struct dog - such structure, wow
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t -Typedef for struct dog
-*/
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
-#endif /* struct dog */
+#endif /* _dog_h_ */
